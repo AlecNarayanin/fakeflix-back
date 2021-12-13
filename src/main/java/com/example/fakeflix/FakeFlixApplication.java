7d6a2c705a4a86d2cfe2ca8994575211a5177594
Alec Narayanin <alec.narayanin@gonexa.fr>
2021-12-13 11:29:00 +0100

    Initial commit

---
+++ src/main/java/com/example/fakeflix/FakeFlixApplication.java
@@ -0,0 +1,13 @@
+package com.example.fakeflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeFlixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeFlixApplication.class, args);
+    }
+
+}
