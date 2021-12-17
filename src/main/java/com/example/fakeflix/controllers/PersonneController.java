a6929042eb1e673cb48d09c3e7f3e93b0b8f67da
Alec Narayanin <alec.narayanin@gonexa.fr>
2021-12-17 09:37:23 +0100

    Maj et usage des DTO

--- src/main/java/com/example/fakeflix/controllers/PersonneController.java
+++ src/main/java/com/example/fakeflix/controllers/PersonneController.java
@@ -1,15 +1,10 @@
 package com.example.fakeflix.controllers;
-
-import com.example.fakeflix.entities.Film;
 import com.example.fakeflix.entities.Personne;
-import com.example.fakeflix.entities.Utilisateur;
-import com.example.fakeflix.models.user.UpsertDTO;
-import com.example.fakeflix.services.FilmService;
+import com.example.fakeflix.models.person.RegisterDTO;
+import com.example.fakeflix.models.person.UpsertDTO;
 import com.example.fakeflix.services.PersonneService;
-import com.example.fakeflix.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,20 +15,22 @@ public class PersonneController {
 
     @Operation(summary = "Retourne une personne")
     @RequestMapping(value = "/person/{id}", method = RequestMethod.GET)
-    public Personne getPerson(@PathVariable(value = "id") Integer id) throws Exception {
+    public Personne getPerson(@PathVariable(value = "id") Integer id) {
         return personneService.getById(id);
     }
 
     @Operation(summary = "Créer une personne")
     @RequestMapping(value ="/person" , method = RequestMethod.POST)
-    public Personne createPersonne(@RequestBody Personne person){
-       return personneService.create(person);
+    public Personne createPersonne(@RequestBody RegisterDTO registerDTO){
+        Personne p = registerDTO.toModel();
+        return personneService.create(p);
     }
 
     @Operation(summary = "Mettre à jour une personne")
     @RequestMapping(value ="/person" , method = RequestMethod.PUT)
-    public void updatePersonne(@RequestBody Personne person) throws Exception{
-        boolean operationResult = personneService.update(person);
+    public void updatePersonne(@RequestBody UpsertDTO upsertDTO) throws Exception{
+        Personne p = upsertDTO.mapPersonModelFromDTO(personneService.getById(upsertDTO.getId()));
+        boolean operationResult = personneService.update(p);
         if(!operationResult){
             throw new Exception("Personne inexistante");
         }
--- src/main/java/com/example/fakeflix/models/person/UpsertDTO.java
+++ src/main/java/com/example/fakeflix/models/person/UpsertDTO.java
@@ -1,54 +1,57 @@
 package com.example.fakeflix.models.person;
 
+import com.example.fakeflix.entities.Personne;
 import com.example.fakeflix.entities.Utilisateur;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class UpsertDTO implements Serializable {
 
-    private Integer Id;
-    private String Username;
-    private String email;
-    private String Password;
+    private Integer id;
 
+    private String firstname;
+
+    private String lastname;
+
+    private Date birthday;
 
     public Integer getId() {
-        return Id;
+        return id;
     }
 
     public void setId(Integer id) {
-        Id = id;
+        this.id = id;
     }
 
-    public String getUsername() {
-        return Username;
+    public String getFirstname() {
+        return firstname;
     }
 
-    public void setUsername(String username) {
-        Username = username;
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
     }
 
-    public String getEmail() {
-        return email;
+    public String getLastname() {
+        return lastname;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
     }
 
-    public String getPassword() {
-        return Password;
+    public Date getBirthday() {
+        return birthday;
     }
 
-    public void setPassword(String password) {
-        Password = password;
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
     }
 
-
-    public Utilisateur MapUserModelFromDTO(Utilisateur u){
-        u.setNomUtilisateur(this.getUsername());
-        u.setEmail(this.getEmail());
-        return u;
+    public Personne mapPersonModelFromDTO(Personne p){
+        p.setFirstname(this.getFirstname());
+        p.setLastname(this.getLastname());
+        p.setBirthday(this.getBirthday());
+        return p;
     }
-
 }
